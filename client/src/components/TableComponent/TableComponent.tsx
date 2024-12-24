9ae1e7930f47804c926539ed696cf93b92634c30
HudorHasHasHa <jankowskiigor5@gmail.com>
2024-12-24 15:03:48 +0100

    add basic tableComponent

---
+++ client/src/components/TableComponent/TableComponent.tsx
@@ -0,0 +1,28 @@
+export interface TableComponentProps<T> {
+    items: T[];
+    tableHeaders: string[];
+    alignLeft?: boolean;
+}
+
+export default function TableComponent<T>({
+    items,
+    tableHeaders,
+    alignLeft = false,
+}: TableComponentProps<T>) {
+    return (
+        <table>
+            <thead>
+                {tableHeaders.map((header) => (
+                    <th>{header}</th>
+                ))}
+            </thead>
+            <tbody>
+                {items.map((item) => (
+                    <tr></tr>
+                ))}
+            </tbody>
+            {/* <tfoot> not sure if i'll be using it for now
+            </tfoot> */}
+        </table>
+    );
+}

67e08d4cfd39101f1c12eda1f985f0a62ac220b6
HudorHasHasHa <jankowskiigor5@gmail.com>
2025-01-23 16:44:33 +0100

    add basic add stock button under table

--- client/src/components/TableComponent/TableComponent.tsx
+++ client/src/components/TableComponent/TableComponent.tsx
@@ -7,23 +7,26 @@ export default function TableComponent<T extends Record<string, any>>({
     useChildrenContent = false,
 }: TableComponentProps<T>) {
     return (
-        <table className="table table-striped table-hover table-bordered no-top-border w-100">
-            <thead>
-                {tableHeaders.map((header) => (
-                    <th className="no-top-border">{header}</th>
-                ))}
-            </thead>
-            <tbody>
-                {children && useChildrenContent
-                    ? children
-                    : tableItems.map((item, rowIndex) => (
-                          <tr key={rowIndex}>
-                              {Object.keys(item).map((key, cellIndex) => (
-                                  <td key={cellIndex}>{item[key]}</td>
-                              ))}
-                          </tr>
-                      ))}
-            </tbody>
-        </table>
+        <div className="d-flex flex-column align-items-end">
+            <table className="table table-striped table-hover table-bordered no-top-border w-100 mb-1">
+                <thead>
+                    {tableHeaders.map((header) => (
+                        <th className="no-top-border">{header}</th>
+                    ))}
+                </thead>
+                <tbody>
+                    {children && useChildrenContent
+                        ? children
+                        : tableItems.map((item, rowIndex) => (
+                              <tr key={rowIndex}>
+                                  {Object.keys(item).map((key, cellIndex) => (
+                                      <td key={cellIndex}>{item[key]}</td>
+                                  ))}
+                              </tr>
+                          ))}
+                </tbody>
+            </table>
+            <button className="btn btn-primary col-2">Add</button>
+        </div>
     );
 }
